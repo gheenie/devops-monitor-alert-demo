2036b0cbb32f22b22e8e0983be0140f8174ae455
infoomics <sbx_94@hotmail.com>
2023-03-13 14:22:29 +0000

    add more specific alarms to complete extension task 1

--- terraform/alarm.tf
+++ terraform/alarm.tf
@@ -25,3 +25,59 @@ resource "aws_cloudwatch_metric_alarm" "alert_errors" {
     # After running deploy.sh, copy SNS topic ARN here.
     alarm_actions             = ["arn:aws:sns:us-east-1:921693990905:test-error-alerts"]
 }
+
+resource "aws_cloudwatch_log_metric_filter" "multipleofthree_error" {
+    log_group_name = "/aws/lambda/${aws_lambda_function.error_lambda.function_name}"
+
+    pattern        = "MultipleOfThreeError"
+    name           = "MultipleOfThreeErrorFilter"
+
+    metric_transformation {
+        namespace = "CustomLambdaMetrics"
+        name      = "MultipleOfThreeErrorCount"
+        value     = "1"
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alert_multipleofthree_errors" {
+    namespace                 = aws_cloudwatch_log_metric_filter.multipleofthree_error.metric_transformation[0].namespace
+    metric_name               = aws_cloudwatch_log_metric_filter.multipleofthree_error.metric_transformation[0].name
+
+    alarm_name                = "AlertMultipleOfThreeErrors"
+    statistic                 = "Sum"
+    period                    = "60"
+    comparison_operator       = "GreaterThanOrEqualToThreshold"
+    threshold                 = "1"
+    evaluation_periods        = "1"
+
+    # After running deploy.sh, copy SNS topic ARN here.
+    alarm_actions             = ["arn:aws:sns:us-east-1:921693990905:test-error-alerts"]
+}
+
+resource "aws_cloudwatch_log_metric_filter" "runtime_error" {
+    log_group_name = "/aws/lambda/${aws_lambda_function.error_lambda.function_name}"
+
+    pattern        = "RuntimeError"
+    name           = "RuntimeErrorFilter"
+
+    metric_transformation {
+        namespace = "CustomLambdaMetrics"
+        name      = "RuntimeErrorCount"
+        value     = "1"
+    }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alert_runtime_errors" {
+    namespace                 = aws_cloudwatch_log_metric_filter.runtime_error.metric_transformation[0].namespace
+    metric_name               = aws_cloudwatch_log_metric_filter.runtime_error.metric_transformation[0].name
+
+    alarm_name                = "AlertRuntimeErrors"
+    statistic                 = "Sum"
+    period                    = "60"
+    comparison_operator       = "GreaterThanOrEqualToThreshold"
+    threshold                 = "1"
+    evaluation_periods        = "1"
+
+    # After running deploy.sh, copy SNS topic ARN here.
+    alarm_actions             = ["arn:aws:sns:us-east-1:921693990905:test-error-alerts"]
+}
